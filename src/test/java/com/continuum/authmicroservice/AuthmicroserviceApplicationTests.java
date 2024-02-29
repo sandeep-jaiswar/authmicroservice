d62f458140e8ea6f57c13019be2b7d8e0b12d830
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2024-02-29 14:20:09 +0530

    initialised project with spring intializer

---
+++ src/test/java/com/continuum/authmicroservice/AuthmicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.continuum.authmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthmicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
